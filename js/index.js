0f69dcb5c2af9659c0db9d19cbf305b2c03d913e
SaraNagahara <sarasfnagahara@gmail.com>
2025-06-05 11:19:24 -0300

    atualizando nav, mensagens e melhorando a responsividade

---
+++ js/index.js
@@ -0,0 +1,22 @@
+
+/*Button Clicks */
+
+document.querySelector(".certificado").addEventListener('click', function() {
+    window.open('https://www.freecodecamp.org/SaraNagahara', '_blank');
+});
+
+document.querySelector(".github-logo").addEventListener('click' , function(){
+    window.open('https://github.com/SaraNagahara', '_blank');
+});
+
+document.querySelector(".code-logo").addEventListener('click' , function(){
+    window.open('https://codepen.io/SaraNagahara', '_blank');
+});
+
+/*email */
+
+function enviarEmail() {
+    window.location.href = 'mailto:sarasfnagahara@gmail.com';
+}
+
+
